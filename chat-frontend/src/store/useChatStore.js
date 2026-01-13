992f0ca4757089d2ac5f72e115947756f048d06f
nimaseroshan9 <nimaseroshan9@gmail.com>
2026-01-13 12:03:43 +0530

    canges made

--- chat-frontend/src/store/useChatStore.js
+++ chat-frontend/src/store/useChatStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = (import.meta.env.VITE_BACKEND_URL || (import.meta.env.MODE === 'production' ? window.location.origin : 'http://localhost:3001')).replace(/\/$/, '');
+const SOCKET_URL = (import.meta.env.VITE_BACKEND_URL || 'http://localhost:3001').replace(/\/$/, '');
 
 export const useChatStore = create((set, get) => ({
   // --- STATE ---
